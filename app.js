b34bfbe5918c5d39ab76132e9689da6598ce1493
Corey McCue <coreysmccue@gmail.com>
2021-02-11 13:05:08 +0100

    add map API project to repo

---
+++ app.js
@@ -0,0 +1,28 @@
+import mapboxgl from 'mapbox-gl';
+
+const searchLocation = document.querySelector('#searchLocation');
+const searchRequest = document.querySelector('#searchBox');
+const lat = document.querySelector('#lat');
+const long = document.querySelector('#long');
+
+searchLocation.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // console.log(searchRequest.value);
+  fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchRequest.value}.json?access_token=pk.eyJ1IjoidGhlY29yZXltY2N1ZSIsImEiOiJja2tzYnRobnUxMXU0Mm9xbnFvNHkwbXVqIn0.qWWSsCZufFF1eBUzhcEKnw`)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data.features[0].center);
+      lat.innerHTML = `${data.features[0].center[1]}˚`;
+      long.innerHTML = `${data.features[0].center[0]}˚`;
+      mapboxgl.accessToken = 'pk.eyJ1IjoidGhlY29yZXltY2N1ZSIsImEiOiJja2tzYnRobnUxMXU0Mm9xbnFvNHkwbXVqIn0.qWWSsCZufFF1eBUzhcEKnw';
+      const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        center: [data.features[0].center[0], data.features[0].center[1]],
+        zoom: 12
+      });
+      new mapboxgl.Marker()
+        .setLngLat([data.features[0].center[0], data.features[0].center[1]])
+        .addTo(map);
+    });
+});
